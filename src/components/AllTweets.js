9f28a088ec59bd30615675466f3cf49db396db6b
Chris <cms5377@uncw.edu>
2022-07-22 10:30:01 -0400

    working with commit

--- src/components/AllTweets.js
+++ src/components/AllTweets.js
@@ -3,28 +3,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Like from "../components/Like"
-<<<<<<< HEAD
 import Delete from "./Delete";
 import Comment from "./Comment";
 
-=======
-import '../App.css'
->>>>>>> 8b9ab5ac367bca600ea36071bd750e18ad1a3eb5
 
 function AllTweets() {
   const [tweets, setTweets] = useState([]);
-
-useEffect(() => {
-   
-  axios.get('http://localhost:8080/user', {
-    headers: {
-      Authorization: 'Bearer ' +  localStorage.getItem("token")
-    }} ).then(
-      res => {console.log(res)},
-      err => {console.log(err)}
-    )
-}, []);
-
 
   useEffect(() => {
     axios
@@ -45,7 +29,6 @@ useEffect(() => {
     <div className="tweet-container">
       {tweets.map((tweet) => {
         return (
-<<<<<<< HEAD
           
           <div className="card" key={tweet.id}>
             <h2 > {tweet.user.username} </h2>
@@ -60,42 +43,17 @@ useEffect(() => {
             <Comment tweetid = {tweet.id} commentO = {tweet.comments}/>
             <div className = "comment" key = {tweet.comments.id}>
                 {tweet.comments.map(comment =>
-                  <p>{comment.text}</p>
+                  <div className = "comment1" key = {tweet.comments.id}>
+                    <p>{comment.text}</p>
+                  </div>
                   )}
             </div>
 
-=======
-          // <Link to={`/tweets/${tweet.id}`}>
-            <div className="card" key={tweet._id}>
-            
-              <h2 className="tweet-author"> {tweet.user.username} </h2>
-              <p className="tweet-text">{tweet.text}</p>
-              <p>{tweet.likes.length}</p>
-              <Like />
-            
-            
-
-            <div>
-                {tweet.comments.map(comment =>
-                  <p className="tweet-comments">{comment.text}</p>
-                  )}
-              </div>
->>>>>>> 8b9ab5ac367bca600ea36071bd750e18ad1a3eb5
           </div>
-
-          // </Link>
         );
       })}
-
-      
     </div>
-<<<<<<< HEAD
   )
-=======
-
-
-  );
->>>>>>> 8b9ab5ac367bca600ea36071bd750e18ad1a3eb5
 }
 
 export default AllTweets;
