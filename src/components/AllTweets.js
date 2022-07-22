d57d48166f2632ee84e2c86dab5c5c944f11d5e1
anfalfadhil <anfalfadhil90@gmail.com>
2022-07-22 11:12:02 -0400

    some styles added

--- src/components/AllTweets.js
+++ src/components/AllTweets.js
@@ -31,17 +31,17 @@ function AllTweets() {
         return (
           
           <div className="card" key={tweet.id}>
-            <h2 > {tweet.user.username} </h2>
+            <h2  className="tweet-author"> {tweet.user.username} </h2>
             <Link to={`/tweets/${tweet.id}`}>
 
-              <p >{tweet.text}</p>
+              <p className="tweet-text" >{tweet.text}</p>
 
             </Link>
             {/* <p>{tweet.id}</p> */}
             <Like tweetid = {tweet.id} likes = {tweet.likes}/>
             <Delete tweet = {tweet}/>
             <Comment tweetid = {tweet.id} commentO = {tweet.comments}/>
-            <div className = "comment" key = {tweet.comments.id}>
+            <div className="tweet-author" key = {tweet.comments.id}>
                 {tweet.comments.map(comment =>
                   <div className = "comment1" key = {tweet.comments.id}>
                     <p>{comment.text}</p>
